4a75e7ffc3b842f8a74ce394d039bb992e5d770c
CodeAFox <illeskrisztinacsilla@gmail.com>
2026-02-22 04:17:35 +0100

    Products and their types added

---
+++ src/domain_objects/Product.fs
@@ -0,0 +1,22 @@
+module PCLAssignment.domain_objects.Product
+
+// Types of coffee
+type Coffee =
+    | Latte
+    | Melange
+    | Cappuccino
+
+// Types of tea
+type Tea = 
+    | Oolong
+    | GreenTea
+    | Matcha
+    | BlackTea
+    | WhiteTea
+    | Chai
+
+// Types of juice
+type Juice =
+    | OrangeJuice
+    | AppleJuice
+    | GrapeJuice
