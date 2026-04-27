a9860b488e32478e51b0ffd00ee3fe22b8d9a3bb
CodeAFox <150443592+CodeAFox@users.noreply.github.com>
2026-04-27 11:50:14 +0200

    Minor fix

--- DjangoApp/shop/apps.py
+++ DjangoApp/shop/apps.py
@@ -1,8 +1,11 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from .signals import load_initial_data
 
 
 class ShopConfig(AppConfig):
     name = 'shop'
 
     def ready(self):
-        print("To get all data, run 'python manage.py migrate'")
+        print("To load initial data, run 'python manage.py migrate'")
+        post_migrate.connect(load_initial_data, sender=self)
--- DjangoApp/shop/signals.py
+++ DjangoApp/shop/signals.py
@@ -3,3 +3,4 @@ from django.core.management import call_command
 def load_initial_data(sender, **kwargs):
     print("loaded")
     call_command("loaddata", "categories.json")
+    call_command("loaddata", "products.json")
