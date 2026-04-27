6223b92e2f9122e29408fdcd9459a85a47ab03c5
CodeAFox <150443592+CodeAFox@users.noreply.github.com>
2026-04-27 11:54:47 +0200

    Got rid of a print

--- DjangoApp/shop/signals.py
+++ DjangoApp/shop/signals.py
@@ -1,6 +1,5 @@
 from django.core.management import call_command
 
 def load_initial_data(sender, **kwargs):
-    print("loaded")
     call_command("loaddata", "categories.json")
     call_command("loaddata", "products.json")
