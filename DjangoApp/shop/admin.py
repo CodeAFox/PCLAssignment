fc2e30b403837d0ad5feff2e483f6780a87da14f
CodeAFox <150443592+CodeAFox@users.noreply.github.com>
2026-04-27 09:34:46 +0200

    Minor fixes & comments

--- DjangoApp/shop/admin.py
+++ DjangoApp/shop/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Category, Product, Payment, Order
 # Register your models here.
+# To log in on the admin site, a superuser needs to be created
+# Run the command: 'python manage.py createsuperuser'
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Payment)
